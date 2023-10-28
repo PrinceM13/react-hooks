ea01beed6201134920866160c92df42b397d22b9
Parames Pasitprachaja <parames.pasitprachaja@gmail.com>
2023-10-28 15:02:35 +0700

    challenge: decryption text

--- src/pages/ChallengePage.jsx
+++ src/pages/ChallengePage.jsx
@@ -2,20 +2,64 @@ import { useEffect, useState } from "react";
 
 const useTextDecryption = (data, interval = 3000) => {
   const [currentIndex, setCurrentIndex] = useState(0);
+  const [subText, setSubText] = useState("");
+
+  function generateRandomText(length) {
+    const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    let randomText = "";
+    for (let i = 0; i < length; i++) {
+      const randomIndex = Math.floor(Math.random() * characters.length);
+      randomText += characters.charAt(randomIndex);
+    }
+    return randomText;
+  }
 
   // TODO: rotate index every interval
-  // TODO: generate random text (hint: need another hook)
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setCurrentIndex((currentIndex) => (currentIndex + 1) % data.length);
+    }, interval);
+    return () => clearInterval(intervalId);
+  }, [data.length, interval]);
 
-  return [data[currentIndex], ""];
+  // TODO: generate random text (hint: need another hook)
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setSubText((previous) => data[currentIndex].slice(0, previous.length + 1));
+    }, interval / data[currentIndex].length);
+    return () => clearInterval(intervalId);
+  }, [data.length, interval, currentIndex]);
+
+  useEffect(() => {
+    setSubText("");
+  }, [currentIndex]);
+
+  return [
+    data[currentIndex],
+    subText,
+    generateRandomText(data[currentIndex]?.length - subText?.length + 1)
+  ];
 };
 
 export const ChallengePage = () => {
   const data = ["encrypt access tokens", "share encrypted data", "encrypt credit cards"];
-  const [text, randomText] = useTextDecryption(data);
+  const [fullText, subText, randomText] = useTextDecryption(data);
+
   return (
-    <div>
-      <h1>
-        {text}
+    <div style={{ width: "100vh" }}>
+      <h1
+        style={{
+          textAlign: "start"
+        }}
+      >
+        {fullText}
+      </h1>
+      <h1
+        style={{
+          textAlign: "start"
+        }}
+      >
+        {subText}
         {randomText}
       </h1>
     </div>
